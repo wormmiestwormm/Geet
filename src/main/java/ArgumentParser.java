656d7bf83565cd6c3cd2710e50ac1cfe4085235f
wormmiestwormm <110218392+wormmiestwormm@users.noreply.github.com>
2025-07-10 14:07:56 -0700

    Added most command line commands

--- src/main/java/ArgumentParser.java
+++ src/main/java/ArgumentParser.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class ArgumentParser {
     private String[] args;
     public ArgumentParser(String[] args) {
@@ -5,25 +7,60 @@ public class ArgumentParser {
     }
 
     public void getCommand(){
-        if (args.length < 1) {
-            System.out.println("Error: Command Not Found");
-        }
-        else {
-            if (args[0].contains("-init")) {
-                System.out.println("Initialized geet repository in [path]");
-            }
-            else if (args[0].contains("-add")){
-                System.out.println("Added [path] to commit");
-            }
-            else if (args[0].contains("-commit")){
-                System.out.println("Commited");
-            }
-            else if (args[0].contains("-log")){
-                System.out.println("Log");
-            }
-            else {
-                System.out.println("Error: Command Not Found");
-            }
+        switch(args[0]){
+            case "init":
+                File directory = new File("");
+                String userDirectory = directory.getAbsolutePath();
+                //Initialize geet repository using userDictionary in StagingArea or StorageModule
+                System.out.println("Initialized geet repository in " + userDirectory);
+                break;
+
+            case "add":
+                if (args.length < 2){
+                    System.out.println("Error: No file specified" +
+                            "\nPlease specify the file name: geet.bat add <file>");
+                    break;
+                }
+                File newFile = new File(args[1]);
+
+                if (newFile.exists()) {
+                    System.out.println("Error: file name doesn't exist");
+                    break;
+                }
+                else {
+                    //add to repository in StagingArea
+                    System.out.println("Added " + newFile.getName() + " to commit");
+                    break;
+                }
+
+            case "commit":
+                if (args.length < 2){
+                    System.out.println("Error: Invalid commit" +
+                            "\nPlease add commit comment: geet.bat commit <\"commit message\">");
+                    break;
+                }
+                else {
+                    System.out.println("Commit will be added later");
+                    break;
+                }
+
+            case "log":
+                System.out.println("log will be added later");
+                break;
+
+            case "":
+                System.out.println("-----------------------------------------------------------------------------------" +
+                                   "\nCommand list:" +
+                        "\n\ninit - Initialize geet repository in current directory" +
+                        "\nadd <file> - add listed file to staging area" +
+                        "\ncommit -m <\"commit message\"> - records files in staging area in the repository" +
+                        "\nlog - view commit history" +
+                        "\n\n-----------------------------------------------------------------------------------");
+                break;
+
+            default:
+                System.out.println("Error: Command not found");
+                break;
         }
     }
 }
