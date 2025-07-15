3cdb0d7779da99a536ff0d6e2a9dab9b584753b1
wormmiestwormm <110218392+wormmiestwormm@users.noreply.github.com>
2025-07-14 22:55:54 -0700

    In process of updating add function/StagingArea class

--- src/main/java/StagingArea.java
+++ src/main/java/StagingArea.java
@@ -1,36 +1,48 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Arrays;
+import com.google.gson.*;
 
 public class StagingArea {
     //constructor
-    private static ArrayList<File> stage = new ArrayList<>();
+    private static ArrayList<File> stageList = new ArrayList<>();
     private FileOutputStream stageFile;
 
-    private Path repoPath = Paths.get("geet");
+    private Path repoPath = Paths.get(".geet");
 
-    public StagingArea() throws FileNotFoundException {
-        System.out.println(repoPath);
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public StagingArea() throws IOException {
+        FileWriter writer = new FileWriter(repoPath + "\\" + "stage.json");
     }
 
     //adds file to staging area
     public void addFileToStagingArea(File newFile) throws IOException {
-        stage.add(newFile);
+        stageList.add(newFile);
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("stage.json"));
+            stageList = gson.fromJson(reader, (Type) stageList);
+            reader.close();
 
-        Files.copy(newFile.toPath(),
-                new File(repoPath + "\\" + newFile.getName()).toPath(),
-                StandardCopyOption.REPLACE_EXISTING);
+
+            System.out.println(stageList);
+
+            FileWriter writer = new FileWriter(repoPath + "\\" + "stage.json");
+            gson.toJson(stageList, writer);
+            writer.close();
+        }
+        catch(IOException e) {
+
+        }
     }
 
     //adds files in staging area to repository in storage module
     public void commitChanges(){
-
+        //Files.copy(newFile.toPath(),
+        //        new File(repoPath + "\\" + newFile.getName()).toPath(),
+        //        StandardCopyOption.REPLACE_EXISTING);
     }
 }
